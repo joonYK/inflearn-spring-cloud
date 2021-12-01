9466cb9370615cd9737c36051ca6df941b97de77
JY <kims2606@gmail.com>
2021-12-01 15:48:53 +0900

    discoeryservice 생성

---
+++ discoveryservice/src/test/java/jy/study/inflearn/springcloud/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package jy.study.inflearn.springcloud.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
