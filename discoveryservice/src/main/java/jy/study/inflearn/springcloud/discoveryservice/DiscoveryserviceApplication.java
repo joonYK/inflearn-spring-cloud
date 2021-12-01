726cec6da1754efb7d1fa3545d0f67b124bd73b3
JY <kims2606@gmail.com>
2021-12-01 17:31:26 +0900

    discoveryservice eureka server 설정

--- discoveryservice/src/main/java/jy/study/inflearn/springcloud/discoveryservice/DiscoveryserviceApplication.java
+++ discoveryservice/src/main/java/jy/study/inflearn/springcloud/discoveryservice/DiscoveryserviceApplication.java
@@ -2,8 +2,10 @@ package jy.study.inflearn.springcloud.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryserviceApplication {
 
     public static void main(String[] args) {
