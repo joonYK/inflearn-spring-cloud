801e8d4ad27074be680f4b325552d65cc61b1a6a
JY <kims2606@gmail.com>
2021-12-01 17:48:45 +0900

    user-service project 생성. eureka 서버에 client로 등록 설정.

---
+++ user-service/src/test/java/jy/study/inflearn/springcloud/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jy.study.inflearn.springcloud.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
