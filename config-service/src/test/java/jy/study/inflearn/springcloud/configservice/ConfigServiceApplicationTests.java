042cbad5f7e6a6159b5dc1160abbcf35b66b62b5
JY <kims2606@gmail.com>
2021-12-08 09:56:39 +0900

    config-server 생성

---
+++ config-service/src/test/java/jy/study/inflearn/springcloud/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package jy.study.inflearn.springcloud.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
