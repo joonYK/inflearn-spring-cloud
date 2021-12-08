30a0eeb086f1f0a57b42baaf40f1cacc47a3ef0a
JY <kims2606@gmail.com>
2021-12-08 21:33:15 +0900

    user-service config service에서 설정파일 받아오도록 설정. actuator 적용.

--- user-service/src/main/java/jy/study/inflearn/springcloud/userservice/controller/GatewayController.java
+++ user-service/src/main/java/jy/study/inflearn/springcloud/userservice/controller/GatewayController.java
@@ -16,8 +16,11 @@ public class GatewayController {
 
     @GetMapping("/health-check")
     public String status() {
-        return String.format("It's Working in User Service on PORT %s",
-                env.getProperty("local.server.port"));
+        return "It's Working in User Service" +
+                ", port(local.server.port)= " + env.getProperty("local.server.port") +
+                ", port(server.port)= " + env.getProperty("server.port") +
+                ", token secret= " + env.getProperty("token.secret") +
+                ", token expiration time= " + env.getProperty("token.expiration_time");
     }
 
     @GetMapping("/welcome")
